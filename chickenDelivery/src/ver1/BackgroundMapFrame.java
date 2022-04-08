04c4781bcec28de4d02bce28341ab9b967b11cbb
DESKTOP-D <leeedanny1@gmail.com>
2022-04-08 17:50:05 +0900

    팀프로젝트 치킨 타이쿤

--- chickenDelivery/src/ver1/BackgroundMapFrame.java
+++ chickenDelivery/src/ver1/BackgroundMapFrame.java
@@ -1,5 +1,8 @@
 package ver1;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -22,17 +25,26 @@ public class BackgroundMapFrame extends JFrame{
 		initObject();
 		initSetting();
 		initListener();
+		changePanel();
 		
-		setVisible(true);
-
+		
 	}
 	
 	private void initObject() {
+		setTitle("치킨배달");
+		setSize(800, 800);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		panel1 = new JPanel();
+		changeBtn = new JButton("change");
 	}
 	
 	private void initSetting() {
+		setVisible(true);
+		//panel1.setLayout(new FlowLayout(FlowLayout.));
+		add(panel1);
 		
+		panel1.add(changeBtn);
 	}
 	
 	private void initListener() {
@@ -40,14 +52,14 @@ public class BackgroundMapFrame extends JFrame{
 	}
 	
 	private void changePanel() {
-		changeBtn.addMouseListener(new MouseAdapter() {
+		changeBtn.addActionListener(new ActionListener() {
 			
 			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-				super.mouseClicked(e);
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("화면 전환");
 			}
 		});
 	}
+	
 
 }
--- chickenDelivery/src/ver1/MainTest.java
+++ chickenDelivery/src/ver1/MainTest.java
@@ -3,8 +3,8 @@ package ver1;
 public class MainTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		new BackgroundMapFrame();
 	}
 
 }
