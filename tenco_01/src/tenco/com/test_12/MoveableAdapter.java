46f41a9d87755a3e54613f97eb06b67e7e419b11
DESKTOP-D <leeedanny1@gmail.com>
2022-04-11 12:19:40 +0900

    버블버블 버블생성 및 발사

---
+++ tenco_01/src/tenco/com/test_12/MoveableAdapter.java
@@ -0,0 +1,30 @@
+package tenco.com.test_12;
+
+// 필터 --> 30v -> 10v 설계 수정할 수 있다
+public abstract class MoveableAdapter implements Moveable{
+
+	@Override
+	public void left() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void right() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void up() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void down() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
