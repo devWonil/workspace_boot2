9904cd1cdd8646f63de63d779e6ee387ba08f377
unknown <oneill91@naver.com>
2022-04-17 20:25:18 +0900

    과제

---
+++ tenco_01/src/tenco/com/test_16_1/BubbleFrame.java
@@ -0,0 +1,14 @@
+package tenco.com.test_16_1;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class BubbleFrame extends JFrame{ // JFrame 상속
+
+	// 1단계
+	private BubbleFrame mContext;
+	
+	private JLabel backgroundMap; // JLabel 클래스 멤버변수 선언
+	public Player player;
+	
+}
---
+++ tenco_01/src/tenco/com/test_16_1/PlayerWay.java
@@ -0,0 +1,5 @@
+package tenco.com.test_16_1;
+
+public enum PlayerWay {
+	LEFT, RIGHT // enum 변수는 세미콜론 ;을 찍지않는다
+}
