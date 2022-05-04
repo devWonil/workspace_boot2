e4a8aac72e09c80ae2fdf44ce58cf6aa6ea96dbb
DESKTOP-D <leeedanny1@gmail.com>
2022-05-04 17:55:55 +0900

    java에서 db C R U D

---
+++ db_connect/src/ch02/BuyInfoDao.java
@@ -0,0 +1,8 @@
+package ch02;
+
+public class BuyInfoDao {
+
+	// C R U D
+	
+	// 전체 구매 수량
+}
---
+++ db_connect/src/ch02/MainTest.java
@@ -0,0 +1,21 @@
+package ch02;
+
+import java.util.ArrayList;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		
+		MemberInfoDao memberInfoDao = new MemberInfoDao();
+		ArrayList<MemberDto> data = memberInfoDao.select();
+		System.out.println(data);
+		
+		// object -->
+		MemberDto dto = new MemberDto("abc", "강감찬", "서울시 동작구");
+		//memberInfoDao.insert(dto);
+		//memberInfoDao.update(dto);
+		int returnRow = memberInfoDao.delete("abc");
+		System.out.println("returnRow : " + returnRow);
+	}
+
+}
